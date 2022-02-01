9638af2f7ea022179d0fd6926b1db2b3a8f288f9
Douglas <bryandouglasxd@hotmail.com>
2022-02-01 09:23:31 -0300

    adding experience bar component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,18 @@
 <template>
-	<div />
+	<div class="min-h-screen max-w-xl mx-auto flex-col px-5 py-8 md:py-10">
+		<ExperienceBar />
+		<Nuxt />
+	</div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import ExperienceBar from '../components/atoms/ExperienceBar.vue';
+
+export default Vue.extend({
+	name: 'Default',
+	components: {
+		ExperienceBar,
+	},
+});
+</script>
